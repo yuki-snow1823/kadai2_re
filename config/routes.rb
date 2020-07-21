e2b72ea2b12e6a5b6aaf67dca056436a55b3e540
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-22 00:42:04 +0900

    add:replace(jsに関して6と5系で変更点があったためコメントアウトで修正)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'home#top'
+  get 'books' => 'books#index', as:'index_book'
+  get 'books/show' => 'books#show'
+  get 'books/edit' => 'books#edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show' , as: 'book'
 end
